48858fae56b62c917820f0abb2d41e9609de3c85
anshul bansal <akbt.tech17@gmail.com>
2022-08-11 12:14:58 +0530

    added area method

--- MyAreas/Areas.cs
+++ MyAreas/Areas.cs
@@ -4,5 +4,9 @@ namespace MyAreas
 {
     public class Areas
     {
+        public float Circle(int radius) {
+            float area = (float)Math.PI * radius * radius;            
+            return area;
+        }
     }
 }
