c8f6d8cd8d117e37083abb10bf96d873167536df
anshul bansal <akbt.tech17@gmail.com>
2022-08-11 14:47:22 +0530

    added test for circle and triangle

--- TestMyAreas/UnitTest1.cs
+++ TestMyAreas/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using MyAreas;
 
 namespace TestMyAreas
 {
@@ -7,12 +8,43 @@ namespace TestMyAreas
         [SetUp]
         public void Setup()
         {
+            // like constructor of the test
         }
 
         [Test]
         public void Test1()
         {
             Assert.Pass();
+        }
+
+        [Test]
+        public void CicleTest()
+        {
+            // [Test] is important
+            // return type is pulic generally
+            // 1 - Arrange
+            Areas areas = new Areas();
+            float expectedResult = 75;
+            float actualResult = 0;
+
+            // 2 - Act 
+            actualResult = areas.Circle(5);
+            // 3 - Assert
+            Assert.AreEqual(expectedResult, actualResult,5,"Areas not matching");
+      
+        }
+
+        [Test]
+        public void TriangleTest() {
+            // 1 - Arrange
+            Areas areas = new Areas();
+            float expectedResult = 10;
+            float actualResult = 0;
+            // 2 - Act 
+            actualResult = areas.Triangle(5,4);
+            // 3 - Assert
+            Assert.AreEqual(expectedResult, actualResult, "Areas not matching");
+
         }
     }
 }
